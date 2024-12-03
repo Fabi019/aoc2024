e1798638509b7dbb8ec3aae826b67e49a368dba5
Fabi019 <fabi019@gmx.de>
2024-12-03 17:12:55 +0100

    refactor day 3

--- benches/bench.rs
+++ benches/bench.rs
@@ -6,7 +6,8 @@ use criterion::{criterion_group, criterion_main};
 
 aoc2024::bench!(day01);
 aoc2024::bench!(day02);
+aoc2024::bench!(day03);
 
-criterion_group!(benches, day01::bench, day02::bench);
+criterion_group!(benches, day01::bench, day02::bench, day03::bench);
 
 criterion_main!(benches);
--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -3,49 +3,52 @@ use regex::Regex;
 aoc2024::main!("../../assets/day03.txt");
 
 fn part1(input: &str) -> u32 {
-    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    input
+        .match_indices("mul(")
+        .filter_map(|(i, _)| {
+            if let Some(colon) = &input[i..].find(",") {
+                if let Ok(a) = &input[i + 4..i + colon].parse::<u32>() {
+                    if let Some(par) = &input[i + colon..].find(")") {
+                        if let Ok(b) = &input[i + colon + 1..i + colon + par].parse::<u32>() {
+                            return Some(a * b);
+                        }
+                    }
+                }
+            }
 
-    let mut sum = 0;
-
-    for c in re.captures_iter(input) {
-        let first = c.get(1).unwrap().as_str().parse::<u32>().unwrap();
-        let second = c.get(2).unwrap().as_str().parse::<u32>().unwrap();
-
-        sum += first * second;
-    }
-
-    sum
+            None
+        })
+        .sum::<u32>()
 }
 
 fn part2(input: &str) -> u32 {
     let re = Regex::new(r"(mul|do|don't)\((?:(\d+),(\d+))?\)").unwrap();
 
-    let mut sum = 0;
-    let mut enabled = true;
+    re.captures_iter(input)
+        .fold((true, 0), |(enabled, acc), c| {
+            let op = c.get(1).unwrap();
 
-    for c in re.captures_iter(input) {
-        let op = c.get(1).unwrap();
-
-        match op.as_str() {
-            "mul" if enabled => {
-                let a = c.get(2).unwrap().as_str().parse::<u32>().unwrap();
-                let b = c.get(3).unwrap().as_str().parse::<u32>().unwrap();
-                sum += a * b;
+            match op.as_str() {
+                "mul" if enabled => {
+                    let a = c.get(2).unwrap().as_str().parse::<u32>().unwrap();
+                    let b = c.get(3).unwrap().as_str().parse::<u32>().unwrap();
+                    (enabled, acc + a * b)
+                }
+                "do" => (true, acc),
+                "don't" => (false, acc),
+                _ => (enabled, acc),
             }
-            "do" => enabled = true,
-            "don't" => enabled = false,
-            _ => {}
-        }
-    }
-
-    sum
+        })
+        .1
 }
 
 aoc2024::test!(
     "\
 xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
-", 161,
+",
+    161,
     "\
 xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
-", 48
+",
+    48
 );
