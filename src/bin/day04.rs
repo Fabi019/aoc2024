fd2f4acedc444740c5d9f4b195828977141c632b
Fabi019 <fabi019@gmx.de>
2024-12-04 17:39:59 +0100

    refactor day 4

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -1,41 +1,35 @@
 aoc2024::main!("../../assets/day04.txt");
 
 fn part1(input: &str) -> u32 {
-    let grid = input
-        .lines()
-        .map(|l| l.chars().collect::<Vec<_>>())
-        .collect::<Vec<_>>();
+    let grid = input.lines().map(|l| l.as_bytes()).collect::<Vec<_>>();
 
     let mut count = 0;
 
     for y in 0..grid.len() {
         for x in 0..grid[0].len() {
-            let c = grid[y][x];
-            if c != 'X' {
+            if grid[y][x] != b'X' {
                 continue;
             }
 
             for dy in -1..=1 {
-                'outer: for dx in -1..=1 {
+                if dy == -1 && y < 3 || dy == 1 && y >= grid.len() - 3 {
+                    continue;
+                }
+
+                'next: for dx in -1..=1 {
                     if dx == 0 && dy == 0 {
                         continue;
                     }
 
-                    let mut cx = x as isize + dx;
-                    let mut cy = y as isize + dy;
+                    if dx == -1 && x < 3 || dx == 1 && x >= grid[0].len() - 3 {
+                        continue;
+                    }
 
-                    for i in "MAS".chars() {
-                        if cx < 0
-                            || cy < 0
-                            || cx >= grid[0].len() as isize
-                            || cy >= grid.len() as isize
-                        {
-                            continue 'outer;
-                        }
+                    let (mut cx, mut cy) = (x as isize + dx, y as isize + dy);
 
-                        let c = grid[cy as usize][cx as usize];
-                        if c != i {
-                            continue 'outer;
+                    for &i in b"MAS" {
+                        if grid[cy as usize][cx as usize] != i {
+                            continue 'next;
                         }
 
                         cx += dx;
@@ -52,17 +46,13 @@ fn part1(input: &str) -> u32 {
 }
 
 fn part2(input: &str) -> u32 {
-    let grid = input
-        .lines()
-        .map(|l| l.chars().collect::<Vec<_>>())
-        .collect::<Vec<_>>();
+    let grid = input.lines().map(|l| l.as_bytes()).collect::<Vec<_>>();
 
     let mut count = 0;
 
     for y in 0..grid.len() {
-        'outer: for x in 0..grid[0].len() {
-            let c = grid[y][x];
-            if c != 'A' {
+        'next: for x in 0..grid[0].len() {
+            if grid[y][x] != b'A' {
                 continue;
             }
 
@@ -74,11 +64,11 @@ fn part2(input: &str) -> u32 {
                 let (xa, ya) = (x as isize + xa, y as isize + ya);
                 let (xb, yb) = (x as isize + xb, y as isize + yb);
 
-                let first = grid[ya as usize][xa as usize];
-                let second = grid[yb as usize][xb as usize];
+                let a = grid[ya as usize][xa as usize];
+                let b = grid[yb as usize][xb as usize];
 
-                if !(first == 'S' && second == 'M' || first == 'M' && second == 'S') {
-                    continue 'outer;
+                if !(a == b'S' && b == b'M' || a == b'M' && b == b'S') {
+                    continue 'next;
                 }
             }
 
