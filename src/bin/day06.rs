359aff5911193e66ba4344d7bd0ee8ee6b049098
Fabi019 <fabi019@gmx.de>
2024-12-07 11:59:36 +0100

    refactor day 6

--- src/bin/day06.rs
+++ src/bin/day06.rs
@@ -1,16 +1,15 @@
 use std::{collections::HashSet, hash::Hash};
 
 #[derive(PartialEq, Eq)]
-struct PosDir((usize, usize), (isize, isize));
+struct PosDir((usize, usize), usize);
 
 impl Hash for PosDir {
     fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        state.write_u8(self.0.0 as u8);
-        state.write_u8(self.0.1 as u8);
-        state.write_u8(self.1.0 as u8);
-        state.write_u8(self.1.1 as u8);
+        state.write_u32((self.0.0 as u32) << 16 | (self.0.1 as u32) << 8 | self.1 as u32);
     }
 }
+
+const DIRECTIONS: [(isize, isize); 4] = [(0, -1), (1, 0), (0, 1), (-1, 0)];
 
 aoc2024::main!("../../assets/day06.txt");
 
@@ -28,7 +27,7 @@ fn part1(input: &str) -> u32 {
         }
     }
 
-    simulate(&grid, start, (0, -1), (usize::MAX, usize::MAX))
+    simulate(&grid, start, 0, (usize::MAX, usize::MAX))
         .unwrap()
         .len() as u32
 }
@@ -36,41 +35,42 @@ fn part1(input: &str) -> u32 {
 fn simulate(
     grid: &[&[u8]],
     start @ (mut gx, mut gy): (usize, usize),
-    idir @ (mut dx, mut dy): (isize, isize),
-    (ox, oy): (usize, usize),
-) -> Result<HashSet<PosDir>, ()> {
+    mut dir: usize,
+    opos @ (ox, oy): (usize, usize),
+) -> Option<HashSet<PosDir>> {
     let mut visited = HashSet::new();
-    visited.insert(PosDir(start, idir));
+    visited.insert(PosDir(start, dir));
 
     loop {
+        let (dx, dy) = DIRECTIONS[dir];
         let (nx, ny) = (gx as isize + dx, gy as isize + dy);
+
         if nx < 0 || ny < 0 || nx >= grid[0].len() as isize || ny >= grid.len() as isize {
             break;
         }
 
         let np @ (nx, ny) = (nx as usize, ny as usize);
-        if grid[ny][nx] == b'#' || nx == ox && ny == oy {
-            // Turn 90 deg cw
-            let tmp = dx;
-            dx = -dy;
-            dy = tmp;
+
+        if grid[ny][nx] == b'#' || np == opos {
+            dir += 1;
+            dir %= 4;
             continue;
         }
 
         // Only store direction when simulating with additional obstacle
         if ox != usize::MAX && oy != usize::MAX {
-            if !visited.insert(PosDir(np, (dx, dy))) {
-                return Err(()); // Cycle detected
+            if !visited.insert(PosDir(np, dir)) {
+                return None; // Cycle detected
             }
         } else {
-            visited.insert(PosDir(np, idir)); // always use same orientation
+            visited.insert(PosDir(np, 0)); // always use same orientation
         }
 
         gx = nx;
         gy = ny;
     }
 
-    Ok(visited)
+    Some(visited)
 }
 
 fn part2(input: &str) -> u32 {
@@ -87,10 +87,10 @@ fn part2(input: &str) -> u32 {
         }
     }
 
-    simulate(&grid, start, (0, -1), (usize::MAX, usize::MAX))
+    simulate(&grid, start, 0, (usize::MAX, usize::MAX))
         .unwrap()
         .into_iter()
-        .filter_map(|PosDir(pos, _)| simulate(&grid, start, (0, -1), pos).err())
+        .filter(|PosDir(pos, _)| simulate(&grid, start, 0, *pos).is_none())
         .count() as u32
 }
 
