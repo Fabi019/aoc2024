e8d94be1dfd4aae29c53c7dafe2cc3c35437d945
Fabi019 <fabi019@gmx.de>
2024-12-10 20:57:06 +0100

    refactor day 10

--- src/bin/day10.rs
+++ src/bin/day10.rs
@@ -14,37 +14,33 @@ fn part1(input: &str) -> u32 {
         }
     }
 
-    let mut total_score = 0;
-
-    for start in starts {
-        total_score += find_paths(&map, start, true);
-    }
-
-    total_score
+    starts
+        .into_iter()
+        .map(|s| find_paths(&map, s, true))
+        .sum::<u32>()
 }
 
 fn find_paths(map: &[&[u8]], start: (isize, isize), unique: bool) -> u32 {
     let mut visited = HashSet::new();
     let mut count = 0;
 
-    let mut queue: VecDeque<((isize, isize), u32)> = VecDeque::new();
-    queue.push_back((start, 0));
+    let mut queue: VecDeque<((isize, isize), _)> = VecDeque::new();
+    queue.push_back((start, b'0'));
 
     while let Some(((cx, cy), height)) = queue.pop_front() {
         for (dx, dy) in &[(0, 1), (-1, 0), (1, 0), (0, -1)] {
-            let (x, y): (isize, isize) = (cx + dx, cy + dy);
+            let (x, y) = (cx + dx, cy + dy);
             if x < 0 || y < 0 || x >= map[0].len() as isize || y >= map.len() as isize {
                 continue;
             }
 
-            if let Some(h) = (map[y as usize][x as usize] as char).to_digit(10) {
-                if h == height + 1 {
-                    if h == 9 && (!unique || visited.insert((x, y))) {
-                        count += 1;
-                        continue;
-                    }
-                    queue.push_back(((x, y), h));
+            let h = map[y as usize][x as usize];
+            if h == height + 1 {
+                if h == b'9' && (!unique || visited.insert((x, y))) {
+                    count += 1;
+                    continue;
                 }
+                queue.push_back(((x, y), h));
             }
         }
     }
@@ -64,13 +60,10 @@ fn part2(input: &str) -> u32 {
         }
     }
 
-    let mut total_score = 0;
-
-    for start in starts {
-        total_score += find_paths(&map, start, false);
-    }
-
-    total_score
+    starts
+        .into_iter()
+        .map(|s| find_paths(&map, s, false))
+        .sum::<u32>()
 }
 
 aoc2024::test!(
