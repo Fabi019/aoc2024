3417b7c532fed9a17458b49002f326c815517ce5
Fabi019 <fabi019@gmx.de>
2024-12-11 20:39:22 +0100

    refactor day 11

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -2,22 +2,25 @@ use std::collections::HashMap;
 
 aoc2024::main!("../../assets/day11.txt");
 
-fn part1(input: &str) -> usize {
+fn part1(input: &str) -> u64 {
     let mut stones = input
         .lines()
         .next()
         .unwrap()
         .split_ascii_whitespace()
-        .map(|n| n.parse::<u64>().unwrap())
-        .collect::<Vec<_>>();
+        .map(|n| (n.parse::<u64>().unwrap(), 1))
+        .collect::<HashMap<_, _>>();
 
-    for _ in 0..25 {
-        let mut index = 0;
-        loop {
-            let stone = stones[index];
+    blink(&mut stones, 25);
 
+    stones.into_iter().fold(0, |acc, (_, count)| acc + count)
+}
+
+fn blink(stones: &mut HashMap<u64, u64>, times: usize) {
+    for _ in 0..times {
+        for (stone, count) in stones.drain().collect::<Vec<_>>() {
             if stone == 0 {
-                stones[index] = 1;
+                *stones.entry(1).or_default() += count;
             } else {
                 let mut digits = 1;
                 let mut offset = 10;
@@ -25,25 +28,17 @@ fn part1(input: &str) -> usize {
                     digits += 1;
                     offset *= 10;
                 }
-    
-                if digits % 2 == 0 {
-                    let divisor = 10u64.pow((digits / 2) as u32);
-                    stones[index] = stone / divisor;
-                    stones.insert(index + 1, stone % divisor);
-                    index += 1;
-                } else {
-                    stones[index] = stone * 2024;
-                }
-            }
 
-            index += 1;
-            if index >= stones.len() {
-                break;
+                if digits % 2 == 0 {
+                    let divisor = 10u64.pow(digits / 2);
+                    *stones.entry(stone / divisor).or_default() += count;
+                    *stones.entry(stone % divisor).or_default() += count;
+                } else {
+                    *stones.entry(stone * 2024).or_default() += count;
+                }
             }
         }
     }
-
-    stones.len()
 }
 
 fn part2(input: &str) -> u64 {
@@ -55,40 +50,9 @@ fn part2(input: &str) -> u64 {
         .map(|n| (n.parse::<u64>().unwrap(), 1))
         .collect::<HashMap<_, _>>();
 
-    for _ in 0..75 {
-        let mut new = HashMap::new();
+    blink(&mut stones, 75);
 
-        for (stone, count) in &stones {
-            if stone == &0 {
-                *new.entry(1).or_default() += count;
-            } else {
-                let mut digits = 1;
-                let mut offset = 10;
-                while stone >= &offset {
-                    digits += 1;
-                    offset *= 10;
-                }
-    
-                if digits % 2 == 0 {
-                    let divisor = 10u64.pow((digits / 2) as u32);
-                    //stones[index] = stone / divisor;
-                    *new.entry(stone / divisor).or_default() += count;
-                    //stones.insert(index + 1, stone % divisor);
-                    *new.entry(stone % divisor).or_default() += count;
-                    //index += 1;
-                } else {
-                    //stones[index] = stone * 2024;
-                    *new.entry(stone * 2024).or_default() += count;
-                }
-            }
-        }
-
-        stones = new;
-    }
-
-    stones.into_iter().fold(0, |acc, (_, count)| {
-        acc + count
-    })
+    stones.into_iter().fold(0, |acc, (_, count)| acc + count)
 }
 
 aoc2024::test!(
@@ -96,7 +60,5 @@ aoc2024::test!(
 125 17
 ",
     55312,
-    "\
-",
-    0
+    65601038650482
 );
