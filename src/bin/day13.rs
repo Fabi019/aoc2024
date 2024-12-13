908d14abd97904826b1004ab1db7d8e3d3275cce
Fabi019 <fabi019@gmx.de>
2024-12-13 18:38:31 +0100

    refactor day 13

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -1,8 +1,8 @@
 aoc2024::main!("../../assets/day13.txt");
 
-fn part1(input: &str) -> u32 {
+fn part1(input: &str) -> i64 {
     let machines = input.split("\n\n").map(|c| {
-        fn parse(s: &str) -> (u32, u32) {
+        fn parse(s: &str) -> (i64, i64) {
             let (x, y) = s.split_once(", ").unwrap();
             (x[2..].parse().unwrap(), y[2..].parse().unwrap())
         }
@@ -11,36 +11,19 @@ fn part1(input: &str) -> u32 {
         let b = l.next().unwrap().split(": ").nth(1).unwrap();
         let p = l.next().unwrap().split(": ").nth(1).unwrap();
         (parse(a), parse(b), parse(p))
-    }).collect::<Vec<_>>();
+    });
 
-    let mut min_tokens = 0;
+    machines.fold(0, |acc, ((ax, ay), (bx, by), (px, py))| {
+        let a = (px * by - bx * py) / (ax * by - bx * ay);
+        let b = (px - ax * a) / bx;
 
-    for ((ax, ay), (bx, by), (px, py)) in machines {
-        let min_bx = px / bx;
-        let min_by = py / by;
-
-        let min = min_bx.min(min_by);
-
-        for i in (0..=min).rev() {
-            let mut current_x = i * bx;
-            let mut current_y = i * by;
-
-            let mut a_count = 0;
-            while current_x <= px && current_y <= py {
-                if current_x == px && current_y == py {
-                    println!("Found min with a * {a_count}, b * {i}");
-                    min_tokens += a_count * 3 + i;
-                    break;
-                }
-                
-                current_x += ax;
-                current_y += ay;
-                a_count += 1;
-            }
+        // validate solution
+        if a * ax + b * bx != px || a * ay + b * by != py {
+            return acc;
         }
-    }
 
-    min_tokens
+        acc + a * 3 + b
+    })
 }
 
 fn part2(input: &str) -> i64 {
@@ -54,11 +37,9 @@ fn part2(input: &str) -> i64 {
         let b = l.next().unwrap().split(": ").nth(1).unwrap();
         let p = l.next().unwrap().split(": ").nth(1).unwrap();
         (parse(a), parse(b), parse(p))
-    }).collect::<Vec<_>>();
+    });
 
-    let mut min_tokens = 0;
-
-    for ((ax, ay), (bx, by), (px, py)) in machines {
+    machines.fold(0, |acc, ((ax, ay), (bx, by), (px, py))| {
         let px = px + 10000000000000;
         let py = py + 10000000000000;
 
@@ -67,13 +48,11 @@ fn part2(input: &str) -> i64 {
 
         // validate solution
         if a * ax + b * bx != px || a * ay + b * by != py {
-            continue;
+            return acc;
         }
 
-        min_tokens += a * 3 + b;
-    }
-
-    min_tokens
+        acc + a * 3 + b
+    })
 }
 
 aoc2024::test!(
@@ -93,5 +72,7 @@ Prize: X=7870, Y=6450
 Button A: X+69, Y+23
 Button B: X+27, Y+71
 Prize: X=18641, Y=10279
-", 480, 875318608908
+",
+    480,
+    875318608908
 );
