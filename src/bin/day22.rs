37194e9d8c80293f71685777274e3f8f271c5cef
Fabi019 <fabi019@gmx.de>
2024-12-22 13:28:47 +0100

    day 22

---
+++ src/bin/day22.rs
@@ -0,0 +1,104 @@
+use std::{
+    arch::x86_64::{
+        __m256i, _mm256_and_si256, _mm256_loadu_si256, _mm256_set1_epi32, _mm256_slli_epi32,
+        _mm256_srli_epi32, _mm256_storeu_si256, _mm256_xor_si256,
+    },
+    collections::{HashMap, HashSet},
+};
+
+aoc2024::main!("../../assets/day22.txt");
+
+fn part1(input: &str) -> usize {
+    let mut numbers = input
+        .lines()
+        .map(|l| l.parse::<u32>().unwrap())
+        .collect::<Vec<_>>();
+
+    numbers
+        .as_mut_slice()
+        .chunks_mut(8)
+        .fold(0, |mut acc, chunk| {
+            let ptr = chunk.as_mut_ptr() as *const __m256i;
+            let mut v = unsafe { _mm256_loadu_si256(ptr) };
+
+            for _ in 0..2000 {
+                v = unsafe { _mm256_xor_si256(v, _mm256_slli_epi32(v, 6)) }; // n ^= n << 6
+                v = unsafe { _mm256_and_si256(v, _mm256_set1_epi32((2 << 23) - 1)) }; // n &= (2 << 23) - 1
+                v = unsafe { _mm256_xor_si256(v, _mm256_srli_epi32(v, 5)) }; // n ^= n >> 5
+                v = unsafe { _mm256_and_si256(v, _mm256_set1_epi32((2 << 23) - 1)) }; // n &= (2 << 23) - 1
+                v = unsafe { _mm256_xor_si256(v, _mm256_slli_epi32(v, 11)) }; // n ^= n << 11
+                v = unsafe { _mm256_and_si256(v, _mm256_set1_epi32((2 << 23) - 1)) }; // n &= (2 << 23) - 1
+            }
+
+            unsafe {
+                _mm256_storeu_si256(ptr as *mut __m256i, v);
+            }
+
+            for i in chunk {
+                acc += *i as usize;
+            }
+            acc
+        })
+}
+
+fn part2(input: &str) -> usize {
+    let numbers = input
+        .lines()
+        .map(|l| l.parse::<usize>().unwrap())
+        .map(|mut n| {
+            let mut prices = Vec::with_capacity(2000 + 1);
+            prices.push(n % 10);
+
+            for _ in 1..=2000 {
+                n ^= n << 6;
+                n &= (2 << 23) - 1;
+
+                n ^= n >> 5;
+                n &= (2 << 23) - 1;
+
+                n ^= n << 11;
+                n &= (2 << 23) - 1;
+
+                prices.push(n % 10);
+            }
+
+            prices
+        });
+
+    let mut sums = HashMap::new();
+
+    for prices in numbers {
+        let changes = prices
+            .windows(2)
+            .map(|ab| ab[1] as isize - ab[0] as isize)
+            .collect::<Vec<_>>();
+        let mut visited = HashSet::new();
+        for (i, seq) in changes.windows(4).enumerate() {
+            // only count first occurence of sequence
+            if !visited.insert(seq) {
+                continue;
+            }
+
+            *sums.entry(seq.to_vec()).or_default() += prices[i + 4];
+        }
+    }
+
+    *sums.values().max().unwrap()
+}
+
+aoc2024::test!(
+    "\
+1
+10
+100
+2024
+",
+    37327623,
+    "\
+1
+2
+3
+2024
+",
+    23
+);
